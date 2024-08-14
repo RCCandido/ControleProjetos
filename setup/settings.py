96206401b7eb3782fdeda3b14e244672ad19f1e3
Rodrigo Cesar Candido <rodrigo.cesar91@yahoo.com.br>
2024-08-14 10:09:13 -0300

    atualizando requirements

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path, os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'rodrigoicmbn@gmail.com'
 
+django_on_heroku.settings(locals())
