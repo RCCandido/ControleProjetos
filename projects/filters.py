44226f2a8b6391719a3c7e81963b106270534178
Rodrigo Cesar Candido <rodrigo.cesar91@yahoo.com.br>
2024-09-20 16:49:50 -0300

    ajuste no filtro contem e placeholder

--- projects/filters.py
+++ projects/filters.py
@@ -4,12 +4,16 @@ from django.db.models import Q
 from .models import Usuario
 
 class UsuarioFilter(django_filters.FilterSet):
-  #name = django_filters.CharFilter(lookup_expr='icontains', label="Name", )
   name = django_filters.CharFilter(method='filterAny')
-
-  def filterAny(self, queryset, name, value):
-    return Usuario.objects.filter(Q(name__in=value) | Q(email__in=(value)))
 
   class Meta:
       model = Usuario
-      fields = '__all__'
+      fields = ['name']
+
+  def filterAny(self, queryset, name, value):
+    return Usuario.objects.filter(
+          Q(name__icontains=value) |
+          Q(email__icontains=value) |
+          Q(tipo__icontains=value) |
+          Q(user_id__icontains=value)
+        )
